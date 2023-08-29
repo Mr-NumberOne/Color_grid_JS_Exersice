9894e0b22bda6ead3c18966729f5d21265e8b91f
Ahmed Maher Ali Al-Maqtari <mr.numberone312@gmail.com>
2023-08-29 23:23:17 +0300

    First Commit

---
+++ main.js
@@ -0,0 +1,9 @@
+var ColorPicked = "lightcoral";
+
+function change_color(currentsquare) {
+  currentsquare.style.backgroundColor = ColorPicked;
+}
+
+function change_picked_color(color) {
+  ColorPicked = color;
+}
